b395d0ec01436901184e0fd3be849a4a149c973a
Usha <ushagb333@gmail.com>
2024-06-23 22:44:02 +0530

    parentChildExample

---
+++ src/app/product/product.component.ts
@@ -0,0 +1,26 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-product',
+  standalone: true,
+  imports: [CommonModule,FormsModule,MatCardModule, MatButtonModule,MatIconModule,RouterLink],
+  templateUrl: './product.component.html',
+  styleUrl: './product.component.css'
+})
+export class ProductComponent {
+[x: string]: any;
+
+  @Input() d:any
+  @Input() p:any
+
+
+
+
+}
+ 
